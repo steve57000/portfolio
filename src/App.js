e5cd6fa6fa71e930b206ec87f838087afe60253a
Bell Steve <steve57b@hotmail.fr>
2024-04-22 10:44:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Router from './Router/Router'; // Importez votre composant Router
+import './App.css';
+
+function App() {
+    return (
+        <Router />
+    );
+}
+
+export default App;
+
+
