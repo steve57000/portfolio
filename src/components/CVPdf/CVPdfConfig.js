cbf0cdbfe3f93bc2b0d112b35f4bb3535a8ff203
Bell Steve <steve57b@hotmail.fr>
2024-07-31 11:44:11 +0200

    suppress file config

--- src/components/CVPdf/CVPdfConfig.js
+++
@@ -1,8 +0,0 @@
-export const CVPdfConfig = {
-    // pagePadding: 20,
-    // sectionMarginBottom: 20,
-    // listMarginLeft: 30,
-    // listItemMarginBottom: 10,
-    // fontWeight: 'bold',
-};
-
